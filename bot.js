73967d168d66d35bdf72e61add440735f5930a14
Frankyx1991 <131958713+Frankyx1991@users.noreply.github.com>
2025-06-07 02:30:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,8 @@
-console.log('Bot funcionando');
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('Hola desde Cerebro IA'));
+
+module.exports = bot;
